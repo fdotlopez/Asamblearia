746f3d2ac9f44f36c5f88ecbb78648e7960f0c05
olgarabassa <rsoler.olga@gmail.com>
2016-07-03 17:07:45 +0200

    reunion mobile

---
+++ mobile/reunion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Asamblearia</title>
+    <link rel="stylesheet" href="mobile/css/master_mobile.css">
+    <link rel="stylesheet" href="css/master_mobile.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>
+
+    </script>
+  </head>
+  <body>
+
+    <?php include_once 'head_interno.html' ?>
+
+    <main id="reunion">
+      <h2>Reunión Trimestral 2</h2>
+
+      <div>
+       <div class="status1">
+         <h4>Reunión</h4>
+       </div>
+       <div class="status2">
+         <h4>Acta</h4>
+       </div>
+      </div>
+
+      <h3 class="titlepage">Aspectos a mejorar</h3>
+      <div class="tagline">
+        <span class="duracion">Duración: 50'</span>
+        <a href="*" class="link">ver índice</a>
+      </div>
+      <article class="box_content">
+        <h5>Descripción</h5>
+        <p>Hace tres meses se marcaron unos claros
+          objetivos para el trimestre.
+          Debemos proponer aspectos a mejorar que
+          reflejen aquellas actividades del día a día
+          que no han salido como estaba panificado o
+          que podrían mejorarse.</p>
+      </article>
+      <article class="box_content">
+        <h5>Dinámica</h5>
+        <p>Durante los 10 primeros minutos comentaremos
+          los fallos que han habido. Después, en grupos de
+          tres personas pensaréis  soluciones. Los últimos
+          15 minutos pondremos en común todas las ideas. </p>
+      </article>
+      <article class="">
+        <h4>Nota</h4>
+        <form action="#" method="post">
+          <label for="descrip">Añade una nota sobre este tema</label>
+          <textarea name="descrip" placeholder="Escribe aquí la nota"></textarea>
+          <input type="submit" value="añadir">
+        </form>
+      </article>
+    </main>
+  </body>
+</html>
